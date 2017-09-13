40705cdd2b09ba4517862bb84b8e8adaf512ad7c
andrew-rayco <andy@andycondon.com>
2017-09-13 22:04:43 +1200

    complete 16

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-function b() {
-  var myVar
-  console.log(myVar)
-}
 
 function a() {
-  var myVar = 2
-  console.log(myVar)
+  function b() {
+    console.log(myVar)
+  }
+  // var myVar = 2
   b()
 }
 
 var myVar = 1
-console.log(myVar)
 a()

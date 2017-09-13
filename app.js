9db117bd43e91ae3728bb13b156b934befb19c05
andrew-rayco <andy@andycondon.com>
2017-09-13 21:35:50 +1200

    completed 10 - undefined

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-b()
+var a
 console.log(a)
 
-var a = 'hello world'
-
-function b() {
-  console.log('Called b!')
+if (a === undefined) {
+  console.log('a is undefined')
+} else {
+  console.log('a is defined')
 }

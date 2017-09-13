0f25441965ed7c2541c2687bef530a6251a65560
andrew-rayco <andy@andycondon.com>
2017-09-13 23:20:25 +1200

    complete 30

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log(libraryName)
+var person = new Object()
+
+person['firstname'] = "Andrew"
+person['lastname'] = "Whatevs"
+
+var firstNameProperty = 'firstname'
+
+console.log(person)
+console.log(person[firstNameProperty])
+
+console.log(person.firstname)
+
+person.address = new Object()
+person.address.street = '111 Main St.'
+person.address.city = 'New York'
+person.address.state = 'NY'
